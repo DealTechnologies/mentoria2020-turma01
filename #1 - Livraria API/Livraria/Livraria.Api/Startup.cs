b774ff628734e8fc103de1b5d15a46256ce31f74
Lucas Santos <lucas.santos@otacom.com.br>
2021-01-18 19:18:42 -0300

    Iniciando api de exemplo

---
+++ #1 - Livraria API/Livraria/Livraria.Api/Startup.cs
@@ -0,0 +1,87 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace Livraria.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            #region AppSettings 
+
+            //services.Configure
+
+            #endregion
+
+            #region [+] Swagger
+
+            services.AddSwaggerGen(c =>
+            {
+                //c.DescribeAllEnumsAsStrings();
+                c.DescribeAllParametersInCamelCase();
+                c.IncludeXmlComments($@"{AppDomain.CurrentDomain.BaseDirectory}\Swagger.xml");
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Livraria API",
+                    Description = "Projeto responsável por gerenciar uma livraria",
+                    Contact = new OpenApiContact 
+                    { 
+                        Name = "Lucas Santos",
+                        Email = "l_santos@hotmail.com.br",
+                        Url = new Uri("http://github.com/lsantoss")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Licença MIT",
+                        Url = new Uri("http://github.com/lsantoss")
+                    }
+                });
+            });
+
+            #endregion
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Livraria API");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
