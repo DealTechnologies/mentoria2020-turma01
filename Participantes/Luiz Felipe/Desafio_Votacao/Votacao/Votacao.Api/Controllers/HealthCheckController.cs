2156ea56c25563fbe2dcd435e7a1de5f493ec064
luiz.moura <luiz.moura@DEALNOTE0081>
2021-01-22 02:19:40 -0300

    HealthCheck - Verificar se a api esta funcionando

---
+++ Participantes/Luiz Felipe/Desafio_Votacao/Votacao/Votacao.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Votacao.Api.Controllers
+{
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    [ApiController]
+    public class HealthCheckController : Controller
+    {
+        /// <summary>
+        /// Health Check
+        /// </summary>
+        /// <remarks><h2>Verifica se a api está funcionando.</h2></remarks>
+        /// <returns code="200"></returns>
+        /// <returns code="500">Internal Server Error</returns>
+        [HttpGet]
+        [Route("v1/healthCheck")]
+        public ActionResult<string> HealthCheck()
+        {
+            try
+            {
+                return "Votação API Ok";
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
