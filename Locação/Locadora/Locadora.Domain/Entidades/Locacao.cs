2ece7bde5778bdf0129c48245b0e4d83b86661ee
luiz.moura <luiz.moura@DEALNOTE0081>
2021-02-10 10:03:28 -0300

    Utilização do EquipamentoPossuiQuantidadeAlugado

--- Locação/Locadora/Locadora.Domain/Entidades/Locacao.cs
+++ Locação/Locadora/Locadora.Domain/Entidades/Locacao.cs
@@ -26,6 +26,7 @@ namespace Locadora.Domain.Entidades
             
             ClientePossuiEndereco();
             EquipamentoPossuiSaldoEstoque();
+            EquipamentoPossuiQuantidadeAlugado();
         }
 
         public bool ClientePossuiEndereco()
